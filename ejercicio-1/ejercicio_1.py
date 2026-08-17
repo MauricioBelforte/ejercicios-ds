e6c35d870d78031b8bc0d9846d95d516f39c0d7e
Mauricio Belforte <belymau@hotmail.com>
2026-08-17 20:11:38 -0300

    Se finalizo el ejercicio-1

---
+++ ejercicio-1/ejercicio_1.py
@@ -0,0 +1,13 @@
+"""
+Ejercicio 1 — ¡Hola Mundo!
+
+Escriba un programa en Python que imprima el mensaje "¡Hola Mundo!".
+"""
+
+
+def main():
+    print("¡Hola Mundo!")
+
+
+if __name__ == "__main__":
+    main()
